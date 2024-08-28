3a107efba744f939f11f4dd95989878a5fffd7c6
Platy <daryl.limkz@gmail.com>
2024-08-28 12:36:46 +0800

    fix(re): added print flag for ironoxide

--- Re/4Fe+3O2/solution/solve.py
+++ Re/4Fe+3O2/solution/solve.py
@@ -23,16 +23,19 @@ p.sendlineafter(
     b"======== Level One ========",
     b"Rust is an iron oxide, a usually reddish-brown oxide formed by the reaction of iron and oxygen in the catalytic presence of water or air moisture."
 )
+p.recvuntil(b"Here is your flag: ")
+log.success(f"Flag 1: {p.recvline().decode().strip()}")
 
 # Level 2
 p.recvuntil(b"Here's your magic number: ")
 magic_number = int(p.recvline())
 fibo = str(fib(magic_number)).encode()
 p.sendline(fibo)
+p.recvuntil(b"Here is your flag: ")
+log.success(f"Flag 2: {p.recvline().decode().strip()}")
 
 # Level 3
 enc = xor(b"tasty", b"Rust was created because the creator had to climb 21 floors.")
 p.sendlineafter(b"======= Level Three =======", enc)
-
-# Get flag
-log.success(p.recvall().decode().strip())
+p.recvuntil(b"Here is your flag: ")
+log.success(f"Flag 3: {p.recvline().decode().strip()}")
